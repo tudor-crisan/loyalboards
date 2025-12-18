d97d032db41d65243b7cbdc1a2dff090d058611b
tudor-crisan <tudor.crisan.webdev@gmail.com>
2025-12-19 01:40:05 +0200

    v.0.0.3

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,8 +1,7 @@
+"use client";
 import { redirect } from "next/navigation";
 import { useAuth } from "@/context/ContextAuth";
-import { getMetadata } from "@/libs/seo";
 
-export const metadata = getMetadata("dashboard");
 export default function LayoutDashboard({ children }) {
   const { isLoggedIn } = useAuth();
 
--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,5 +1,7 @@
 import PagesDashboard from "@/components/pages/PagesDashboard";
+import { getMetadata } from "@/libs/seo";
 
+export const metadata = getMetadata("dashboard");
 export default function Dashboard() {
   return (
     <PagesDashboard />
