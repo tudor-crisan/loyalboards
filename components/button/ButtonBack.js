ecb70b88f62af6fb1ecef16e0e9f64b6877c6d36
tudor-crisan <tudor.crisan.webdev@gmail.com>
2025-12-19 02:21:40 +0200

    v0.0.3

--- components/button/ButtonBack.js
+++ components/button/ButtonBack.js
@@ -4,7 +4,7 @@ import { defaultStyling as styling } from "@/libs/defaults";
 
 export default function ButtonBack({ url = "/", className = "" }) {
   return (
-    <Link href={url} className={`${styling.roundness[0]} ${styling.shadows[0]} btn ${className}`}>
+    <Link href={url} className={`${styling.roundness[0]} btn ${className}`}>
       <SvgBack />
       Back
     </Link>
