d3c542017e20dea2e73d8afcadb6eed40f586c13
tudor-crisan <tudor.crisan.webdev@gmail.com>
2026-01-03 18:40:57 +0200

    0.0.29

--- lists/dashboards.js
+++ lists/dashboards.js
@@ -1,9 +1,7 @@
 import LoyalBoardsDashboard from "@/components/apps/loyalboards/LoyalBoardsDashboard";
-import TaskFlowDashboard from "@/components/apps/taskflow/TaskFlowDashboard";
 
 const dashboards = {
   LoyalBoardsDashboard,
-  TaskFlowDashboard
 };
 
 export default dashboards;
