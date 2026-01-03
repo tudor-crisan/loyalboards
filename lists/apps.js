18f478b0e17fc670927463fcf0a7e9229a50d477
tudor-crisan <tudor.crisan.webdev@gmail.com>
2026-01-03 12:56:12 +0200

    0.0.27

--- lists/apps.js
+++ lists/apps.js
@@ -1,82 +1,8 @@
 const apps = {
   "loyalboards": {
-    "copywriting": {
-      "default": "copywriting0",
-      "override": "lb0_copywriting"
-    },
-    "styling": {
-      "default": "styling0",
-      "override": "lb0_styling"
-    },
-    "visual": {
-      "default": "visual0",
-      "override": "lb0_visual"
-    },
-    "setting": {
-      "default": "setting0",
-      "override": "lb0_setting"
-    }
   },
-  "taskflow": {
-    "copywriting": {
-      "default": "copywriting0",
-      "override": "tf0_copywriting"
-    },
-    "styling": {
-      "default": "styling0",
-      "override": "tf0_styling"
-    },
-    "visual": {
-      "default": "visual0",
-      "override": "tf0_visual"
-    },
-    "setting": {
-      "default": "setting0",
-      "override": "tf0_setting"
-    }
   },
-  "tudorcrisan": {
-    "copywriting": {
-      "default": "copywriting0",
-      "override": "tc0_copywriting"
-    },
-    "styling": {
-      "default": "styling0",
-      "override": "tc0_styling"
-    },
-    "visual": {
-      "default": "visual0",
-      "override": "tc0_visual"
-    },
-    "setting": {
-      "default": "setting0",
-      "override": "tc0_setting"
-    }
   },
-  "contentcalendar": {
-    "copywriting": "copywriting2",
-    "styling": "styling0",
-    "visual": "visual0",
-    "setting": "setting0"
-  },
-  "invoicesnap": {
-    "copywriting": "copywriting3",
-    "styling": "styling0",
-    "visual": "visual0",
-    "setting": "setting0"
-  },
-  "meetingmind": {
-    "copywriting": "copywriting4",
-    "styling": "styling0",
-    "visual": "visual0",
-    "setting": "setting0"
-  },
-  "emailwarmup": {
-    "copywriting": "copywriting5",
-    "styling": "styling0",
-    "visual": "visual0",
-    "setting": "setting0"
-  }
 }
 
 export default apps;
--- lists/copywritings.js
+++ lists/copywritings.js
@@ -1,7 +1,5 @@
 
 import lb0_copywriting from "../data/apps/loyalboards/lb0_copywriting.json";
-import tf0_copywriting from "../data/apps/taskflow/tf0_copywriting.json";
-import tc0_copywriting from "../data/apps/tudorcrisan/tc0_copywriting.json";
 import copywriting0 from "../data/modules/copywriting/copywriting0.json";
 import copywriting1 from "../data/modules/copywriting/copywriting1.json";
 import copywriting2 from "../data/modules/copywriting/copywriting2.json";
@@ -11,8 +9,6 @@ import copywriting5 from "../data/modules/copywriting/copywriting5.json";
 
 const copywritings = {
   lb0_copywriting,
-  tf0_copywriting,
-  tc0_copywriting,
   copywriting0,
   copywriting1,
   copywriting2,
