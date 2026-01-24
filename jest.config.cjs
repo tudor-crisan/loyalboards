9e1743aeadccbecf4aa6c48037d48020bcafd909
tudor-crisan <tudor.crisan.webdev@gmail.com>
2026-01-24 14:07:54 +0200

    0.0.72

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,4 @@
-export default {
+module.exports = {
   testEnvironment: "jest-environment-jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleNameMapper: {
@@ -6,6 +6,9 @@ export default {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
   },
   transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
+    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { configFile: "./babel.config.cjs" }],
   },
+  transformIgnorePatterns: [
+    "/node_modules/(?!(framer-motion|clsx|tailwind-merge|@auth|next-auth)/)",
+  ],
 };
